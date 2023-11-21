305c60ecea4e997a5ba8389f35f71aad343d9edc
DaviidG01 <134561452+DaviidG01@users.noreply.github.com>
2023-11-20 23:10:48 -0500

    Add files via upload

---
+++ user.js
@@ -0,0 +1,29 @@
+//Importar el cliente MongoDB
+const mongo= require('mongodb').MongoClient;
+
+
+// conexion
+const conexion_local="mongodb://localhost:27017/"
+
+
+// Establezco una nueva conexión con mongo
+const cliente=new mongo(conexion_local)
+
+// Indico la Base de Datos
+const dbname="Usuarios"
+
+
+
+const main= async() => {
+    try{
+        // Nos conectamos a MongoDB
+        await cliente.connect();
+        console.log(`He conseguido conectarme a MongoDB users ${conexion_local}`)
+    } catch(fallo){
+        console.error(`He tenido un problema en la operación: ${fallo}`)
+    } finally {
+        await cliente.close()
+    }
+};
+
+main();
